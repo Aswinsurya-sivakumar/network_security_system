87cbb5d9bac790d4fdc6c87548796a67ff374c9e
Aswin Surya <aswinsuryasivakumar2004@gmail.com>
2026-08-15 12:53:49 +0530

    Data Transfromation Completed

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
-from networksecurity.entity.config_entity import TraingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
 if __name__ == "__main__":
     try:
-        trainingpipelineconfig = TraingPipelineConfig()
+        trainingpipelineconfig = TrainingPipelineConfig()
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
@@ -21,7 +22,14 @@ if __name__ == "__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
-           
+        logging.info("data Transformation Started")
+        data_transformation_config = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifict = data_transformation.initiate_data_transformation()
+        print(data_validation_artifact)
+        logging.info("data Transfromation Completed")
+
+
 
 
     except Exception as e:
--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -5,7 +5,7 @@ from networksecurity.constant import training_pipeline
 print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
 
-class TraingPipelineConfig:
+class TrainingPipelineConfig:
     def __init__(self,timestamp = datetime.now()):
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
@@ -14,7 +14,7 @@ class TraingPipelineConfig:
         self.timestamp: str = timestamp
 
 class DataIngestionConfig:
-    def __init__(self,training_pipeline_config:TraingPipelineConfig):
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str = os.path.join(
             training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
         )
@@ -33,7 +33,7 @@ class DataIngestionConfig:
         
 
 class DataValidationConfig:
-    def __init__(self,training_pipeline_config:TraingPipelineConfig):
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
         self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
         self.invalid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
@@ -47,4 +47,13 @@ class DataValidationConfig:
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME,
         )
 
+class DataTransformationConfig:
+     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
+        self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TRAIN_FILE_NAME.replace("csv", "npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir,  training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
+            training_pipeline.TEST_FILE_NAME.replace("csv", "npy"), )
+        self.transformed_object_file_path: str = os.path.join( self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR,
+            training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
         
